5f43d0c1a2e55422c1f6948ccf7fc82cf9280eab
j-dewey <jareddewey9@gmail.com>
2025-06-02 11:53:23 -0400

    Reworking site

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.blog_search, name="blog"),
-    path("<str:post_name>/", views.load_post, name="load_post")
-]
